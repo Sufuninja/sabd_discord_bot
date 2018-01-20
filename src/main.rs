f5c6126c071e8d7953dd4f144a74ea9213494f9e
Omar Quimbaya <omar.quimbaya4@gmail.com>
2018-01-20 12:34:05 -0600

    :panda_face:

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate discord;
+
+use discord::Discord;
+use discord::model::Event;
+use std::env;
+
+
+fn main() {
+    let discord = Discord::from_bot_token(
+        &env::var("DISCORD_TOKEN").expect("Expected token"),
+    ).expect("login failed");
+
+    let (mut connection, _) = discord.connect().expect("connect failed");
+    println!("Ready");
+
+    loop {
+        match connection.recv_event() {
+            Ok(Event::MessageCreate(message)) => {
+                println!("{} says: {}", message.author.name, message.content);
+                if message.content == "!test" {
+                    let _ = discord.send_message(message.channel_id, "This is a reply to the test.", "", false);
+                } else if message.content == "!quit" {
+                    println!("Quitting!");
+                    break
+                }
+            }
+            Ok(_) => {}
+            Err(discord::Error::Closed(code, body)) => {
+                println!("Gateway closed on us with code {:?}: {}", code, body);
+                break
+            }
+            Err(err) => println!("Receive error: {:?}", err)
+        }
+    }
+}
